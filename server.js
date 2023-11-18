badcdffc7ed87d07c74cbb055b7ca90e09097c53
Bob-Karemera-Shema <72112797+Bob-Karemera-Shema@users.noreply.github.com>
2023-11-18 19:01:39 +0000

    knex module

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ import bodyParser from "body-parser";
 import bcrypt from "bcrypt-nodejs";
 import cors from "cors";
 
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 3306,
+      user : 'your_database_user',
+      password : 'your_database_password',
+      database : 'myapp_test'
+    }
+  });
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
