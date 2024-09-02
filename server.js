cce29e600df2437ab8be9cbe4dc2a4142fcfaa79
Bob-Karemera-Shema <72112797+Bob-Karemera-Shema@users.noreply.github.com>
2024-09-02 11:33:49 +0100

    update dependencies

--- server.js
+++ server.js
@@ -14,13 +14,17 @@ app.use(cors());
 const database = knex({
     client: 'pg',
     connection: {
-        connectString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false},
-        host: process.env.DATABASE_HOST,
-        port: 5432,
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PW,
-        database: process.env.DATABASE_DB
+        // connectString: process.env.DATABASE_URL,
+        // ssl: { rejectUnauthorized: false},
+        // host: process.env.DATABASE_HOST,
+        // port: 5432,
+        // user: process.env.DATABASE_USER,
+        // password: process.env.DATABASE_PW,
+        // database: process.env.DATABASE_DB
+        host: '127.0.0.1',
+        user: 'bobsh',
+        password: process.env.pglocalpasskey,
+        database: 'smart-brain'
     }
 });
 
@@ -38,6 +42,6 @@ app.put("/image", (req, res) => image(req, res, database));
 
 app.post("/imageurl", (req, res) => handleAPICall(req, res));
 
-app.listen(process.env.PORT || 3030, () => {
-    console.log(`App running on port ${process.env.PORT}`);
+app.listen(3030, () => {
+    console.log(`App running on port 3030`);
 });
